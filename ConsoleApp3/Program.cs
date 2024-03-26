2642691f957dfe47753a6582cd487fe5923cf861
Usuario <Usuario@CPD03-M10>
2024-03-26 11:30:20 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Escolha uma opção:");
+        Console.WriteLine("1 - Números Ímpares");
+        Console.WriteLine("2 - Números Pares");
+
+        int escolha = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Os números são:");
+          
+        for (int i = escolha; i <= 20; i += 2)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.ReadKey();
+    }
+}
+
+
